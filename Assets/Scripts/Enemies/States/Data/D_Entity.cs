49f71e0222d4493ab0553e4addee727bb112987f
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-09-30 20:20:12 -0700

    Reupload Project Files

---
+++ Assets/Scripts/Enemies/States/Data/D_Entity.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newEntityData", menuName = "Data/Entity Data/Base Data")]
+
+public class D_Entity : ScriptableObject
+{
+    public float maxHealth = 30f;
+
+    public float damageHopSpeed = 3f;
+
+    public float wallCheckDistance = 0.2f;
+    public float ledgeCheckDistance = 0.4f;
+    public float groundCheckRadius = 0.3f;
+    public float stunResistance = 3f;
+    public float stunRecoveryTime = 2f;
+
+    public float minAggroDistance = 3f;
+    public float maxAggroDistance = 4f;
+    public float minAggroCircleRadius = 4f;
+    public float maxAggroCircleRadius = 5f;
+
+    public float closeRangeActionDistance = 1f;
+
+    public GameObject hitParticle;
+
+    public LayerMask whatIsGround;
+    public LayerMask whatIsPlayer;
+}
