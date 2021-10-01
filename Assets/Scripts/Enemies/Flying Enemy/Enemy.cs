49ece1012ea29ebdefd24ca4a075a743ac1201d8
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-09-30 20:57:49 -0700

    Branch Fix For Real

---
+++ Assets/Scripts/Enemies/Flying Enemy/Enemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float upCap;
+    [SerializeField] private float downCap;
+    [SerializeField] private float flightSpeed;
+    private Rigidbody2D rb;
+
+    protected void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.y >= upCap)
+        {
+            rb.velocity = new Vector2(0.0f, -flightSpeed);
+        }
+        else if (transform.position.y <= downCap)
+        {
+            rb.velocity = new Vector2(0.0f, flightSpeed);
+        }
+    }
+
+    public int health = 50;
+
+    public GameObject deathEffect;
+    public GameObject AmmoRefillPrefab;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.gameObject.tag == "Projectile")
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Instantiate(AmmoRefillPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
