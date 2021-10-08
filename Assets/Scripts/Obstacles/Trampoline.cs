59d43ef9c60f6b3e580bee3d16d0b75fa2fedb0e
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-10-07 17:56:05 -0700

    Edited Trampoline Script

--- Assets/Scripts/Obstacles/Trampoline.cs
+++ Assets/Scripts/Obstacles/Trampoline.cs
@@ -4,24 +4,55 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    public float bounce = 25f;
+    public float bounce = 5f;
     private GameObject player;
+    private Animator anim;
+    private bool isPushing;
+    private BoxCollider2D boxCollider;
+
+    [SerializeField] private float pushingTimer = .1f;
+
 
     private void Start()
     {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void Update()
     {
+        anim.SetBool("isPushing", isPushing);
+        if (isPushing)
+        {
+            if (pushingTimer <= 0)
+            {
+                isPushing = false;
+                pushingTimer = .1f;
+            }
+            else
+            {
+                pushingTimer -= Time.deltaTime;
+            }
+        }
     }
 
-    private void OnCollisionEnter2D(Collision2D coll)
+    private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.CompareTag("Player"))
         {
+            isPushing = true;
             player.GetComponent<PlayerController>().dashTimeLeft = 0.0f;
             player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+            boxCollider.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player"))
+        {
+            boxCollider.enabled = true;
         }
     }
 
