aca4afde1259f69f954aa051c0b472f81dc83d6a
JasonZhu95 <68755176+JasonZhu95@users.noreply.github.com>
2021-11-28 22:50:08 -0800

    Added Sfx script

---
+++ Assets/Scripts/Manager/SoundManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerScript : MonoBehaviour
+{
+    //public static AudioClip playerDeath;
+    private static AudioSource audioSrc;
+
+    private void Start()
+    {
+        //playerDeath = Resources.Load<AudioClip>("playerDeath");
+
+        audioSrc = GetComponent<AudioSource>();
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            
+            //case "playerDeath":
+            //    audioSrc.PlayOneShot(playerDeath, 0.1f); 
+            //    break;
+            default:
+                break;
+        }
+    }
+}
